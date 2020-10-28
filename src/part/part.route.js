ccb7d52b0eca87224098fbb35754ffd0edb0ba03
luiswislo <luisfwilson1998@gmail.com>
2020-10-27 17:02:53 -0700

    [part] get part by id

--- src/part/part.route.js
+++ src/part/part.route.js
@@ -1,11 +1,12 @@
 // part.route.js
 import * as express from 'express';
-import { getAllParts } from './part.controller';
+import { getAllParts, getPart } from './part.controller';
 
 const router = express.Router();
 
 // PREFIX: /parts
 
 router.get('/', getAllParts);
+router.get('/:id', getPart);
 
 export default router;
