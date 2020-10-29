b9471ec54c6d930ed8b83e007fdf8c26e951c2bd
miguel0 <miguelbarroe@gmail.com>
2020-10-29 11:11:29 -0700

    [edit-part] can delete componentpart using id

--- src/relationships/component-part/component-part.controller.js
+++ src/relationships/component-part/component-part.controller.js
@@ -5,6 +5,7 @@ export async function deleteComponentPart(req, res) {
 	try {
 		const id = req.params.id;
 		const repo = getRepository(ComponentPart);
+		await repo.delete({ id: id });
 	} catch(err) {
 		console.log(err.message);
 	}
