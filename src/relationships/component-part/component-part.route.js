0023090dfb494f7221b1add38101350deb6bdd20
luiswislo <luisfwilson1998@gmail.com>
2020-11-03 22:04:27 -0700

    [axios] added req & res interceptors

--- src/relationships/component-part/component-part.route.js
+++ src/relationships/component-part/component-part.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { deleteComponentPart } from './component-part.controller';
+import { isAuthenticated } from '../../auth/auth';
 
 const router = express.Router();
 
 // PREFIX: /component-parts
 
-router.delete('/:id', deleteComponentPart);
+router.delete('/:id', isAuthenticated, deleteComponentPart);
 
 export default router;
--- src/vehicle/vehicle.route.js
+++ src/vehicle/vehicle.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { getVehicle, getAllVehicles } from './vehicle.controller';
+import { isAuthenticated } from '../auth/auth';
 
 const router =  express.Router();
 
-router.get('/:id', getVehicle);
-router.get('/', getAllVehicles);
+router.get('/:id', isAuthenticated, getVehicle);
+router.get('/', isAuthenticated, getAllVehicles);
 
 export default router;
