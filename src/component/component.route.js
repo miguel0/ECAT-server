446bf302988f060f6411933caaa6f2e0e79e2dc3
Victor A. Zazueta <A00226702@itesm.mx>
2020-11-04 21:52:00 -0700

    [EDIT-COMPONENT] Proyecto-Final folder name change
    Signed-off-by: Victor A. Zazueta <A00226702@itesm.mx>

--- src/component/component.route.js
+++ src/component/component.route.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { getAllComponents, getComponent, editComponent } from './component.controller';
+
 const router = express.Router();
 
 router.get('/', getAllComponents);
