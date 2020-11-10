f0ca3f7590d7f7a0e7e2fe65dd484c2e111d550d
luiswislo <luisfwilson1998@gmail.com>
2020-11-10 12:35:05 -0700

    [authorization] schema and base implementation

--- src/user/user.route.js
+++ src/user/user.route.js
@@ -1,37 +1,12 @@
 import express from 'express';
-import { getRepository } from 'typeorm';
-import { User } from './user.entity';
+import { isAuthenticated } from '../auth/auth';
+import perms from './user.permissions'; 
+import { getAllUsers, createUser, getUser } from './user.controller';
 
 const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-    try {
-        //return res.status(400).json({message: "Something happened..."});
-        const repo = getRepository(User);
-        const users = await repo.find();
-        res.send(users);
-    } catch(err) {
-        console.log("error here!!");
-        res.send(err.message);
-    }
-    
-});
-
-router.post('/', async (req, res) => {
-    try {
-        let bodyUser = req.body.user;
-        const repo = getRepository(User);
-
-        const newUser = repo.create(bodyUser);
-
-        let savedUser = await repo.save(newUser);
-
-        res.send(savedUser);
-        
-
-    } catch(err) {
-        res.send(err.message);
-    }
-});
+router.post('/', isAuthenticated(perms.add), createUser);
+router.get('/', isAuthenticated(perms.getAll), getAllUsers);
+router.get('/:id', isAuthenticated(perms.get), getUser);
 
 export default router;
