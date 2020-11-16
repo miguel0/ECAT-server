baa13dc7709f8ab5a09c984a89e821fd8f84225e
miguel0 <miguelbarroe@gmail.com>
2020-11-15 17:52:11 -0700

    [edit-component] fix identation

--- src/component/component.controller.js
+++ src/component/component.controller.js
@@ -3,63 +3,63 @@ import { getRepository } from "typeorm";
 import { Component } from "./component.entity";
 
 export async function getAllComponents(req, res) {
-    try {
-        const repo = getRepository(Component);
-        const components = await repo.find();
-        res.send(components);
-    } catch(err) {
-        console.log(err.message);
-    }
-    
+	try {
+		const repo = getRepository(Component);
+		const components = await repo.find();
+		res.send(components);
+	} catch(err) {
+		console.log(err.message);
+	}
+	
 }
 
 export async function getComponent(req, res) {
-    try {
-        let id = req.params.id;
-        const repo = getRepository(Component);
-        const component = await repo.findOneOrFail({id: id}, {relations: ['componentParts', 'componentParts.part']});
-        component.parts = getPrettyParts(component.componentParts);
-        component.componentParts = undefined;
-        res.send(component);
-    } catch(err) {
-        console.log(err.message);
-    }
-    
+	try {
+		let id = req.params.id;
+		const repo = getRepository(Component);
+		const component = await repo.findOneOrFail({id: id}, {relations: ['componentParts', 'componentParts.part']});
+		component.parts = getPrettyParts(component.componentParts);
+		component.componentParts = undefined;
+		res.send(component);
+	} catch(err) {
+		console.log(err.message);
+	}
+	
 }
 
 function getPrettyParts(componentParts) {
-    // TODO: Nest child parts in parents.
-    let parts = [];
-    let temp = {};
+	// TODO: Nest child parts in parents.
+	let parts = [];
+	let temp = {};
 
-    componentParts.forEach(cp => {
+	componentParts.forEach(cp => {
 		temp = cp.part;
 		temp.cpid = cp.id;
-        temp.localQty = cp.localQty;
+		temp.localQty = cp.localQty;
 		temp.remark = cp.remark;
 
 		temp.localNo = getLocalNo(cp, componentParts, '');
 
-        parts.push(temp);
-        temp = {};
-    })
+		parts.push(temp);
+		temp = {};
+	})
 
-    return parts;
+	return parts;
 }
 
 function getLocalNo(cp, list, localNo) {
-    let tempNo = cp.localNo.toString() + localNo;
+	let tempNo = cp.localNo.toString() + localNo;
 
-    if(cp.componentPartId) {
-        for(let i = 0; i < list.length; i++) {
-            if(list[i].id === cp.componentPartId) {
-                return getLocalNo(list[i], list, '.' + tempNo);
-            }
-        }
-        return tempNo;
-    } else {
-        return tempNo;
-    }
+	if(cp.componentPartId) {
+		for(let i = 0; i < list.length; i++) {
+			if(list[i].id === cp.componentPartId) {
+				return getLocalNo(list[i], list, '.' + tempNo);
+			}
+		}
+		return tempNo;
+	} else {
+		return tempNo;
+	}
 }
 
 export async function editComponent(req, res) {
