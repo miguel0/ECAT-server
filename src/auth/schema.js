8ae1960880132e5723a75c9484bc52b89c4467c6
miguel0 <miguelbarroe@gmail.com>
2020-11-17 18:51:39 -0700

    merge conflict

--- src/auth/schema.js
+++ src/auth/schema.js
@@ -1,14 +1,10 @@
 import users from '../user/user.permissions';
 import components from '../component/component.permission';
-<<<<<<< HEAD
-import vehicles from '../vehicle/vehicle.permissions';
-=======
 import parts from '../part/part.permissions';
 import groups from '../group/group.permissions';
 import parts from '../part/part.permissions';
 import vehicles from '../vehicle/vehicle.permissions';
 import compPart from '../relationships/component-part/component-part-permission';
->>>>>>> 2e53613db4477ea3805ca3ccc622a8b409bcb8c6
 
 let baseSchema = {
     C: {
@@ -25,12 +21,9 @@ let baseSchema = {
 			},
 			[components.getAll]: {},
 			[components.get]: {},
-<<<<<<< HEAD
-=======
 			[groups.get]: {},
 			[parts.getAll]: {},
 			[parts.get]: {},
->>>>>>> 2e53613db4477ea3805ca3ccc622a8b409bcb8c6
 			[vehicles.getAll]: {},
 			[vehicles.get]: {}
         },
@@ -52,17 +45,12 @@ let baseSchema = {
 			[users.edit]: {},
 			[users.delete]: {},
 			[components.edit]: {},
-<<<<<<< HEAD
-			[vehicles.add]: {},
-			[vehicles.edit]: {}
-=======
 			[groups.edit]: {},
 			[parts.add]: {},
 			[parts.edit]: {},
 			[vehicles.add]: {},
 			[vehicles.edit]: {},
 			[compPart.delete]: {}
->>>>>>> 2e53613db4477ea3805ca3ccc622a8b409bcb8c6
         }
     }
 }
