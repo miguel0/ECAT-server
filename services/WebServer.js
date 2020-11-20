c69ac30ab7a76716b8dbbe9f7fdda18c791ed7b2
miguel0 <miguelbarroe@gmail.com>
2020-11-20 01:18:53 -0700

    [add-file] bigger json limit

--- services/WebServer.js
+++ services/WebServer.js
@@ -20,7 +20,8 @@ export function initialize() {
         app = express();
         app.use(cors());
         app.use(morgan('combined'))
-        app.use(express.json());
+        app.use(express.json({limit: '10mb'}));
+		app.use(express.urlencoded({limit: '10mb', extended: true}));
         httpServer = http.createServer(app);
         registerRoutes();
         httpServer.listen(config)
