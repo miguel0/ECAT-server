0c367ff1cb3c975595270b477a6d1d4058c189da
luiswislo <luisfwilson1998@gmail.com>
2020-11-26 00:20:53 -0700

    [validation] component-part

--- src/relationships/component-part/component-part.controller.js
+++ src/relationships/component-part/component-part.controller.js
@@ -1,39 +1,51 @@
+import { validationResult } from "express-validator";
 import { getRepository } from "typeorm";
+import { BadBodyError } from "../../exceptions/exceptions";
 import { ComponentPart } from "./component-part.entity";
 
-export async function deleteComponentPart(req, res) {
+export async function deleteComponentPart(req, res, next) {
 	try {
 		const id = req.params.id;
 		const repo = getRepository(ComponentPart);
 		await repo.delete(id);
-		res.send(true);
+		return res.send(true);
 	} catch(err) {
-		res.send(err.message);
+		next(err);
 	}
 }
 
-export async function editComponentPart(req, res) {
+export async function editComponentPart(req, res, next) {
 	try {
+
+		const errors = validationResult(req);
+
+		if (!errors.isEmpty()) {
+			throw new BadBodyError(errors);
+		}
+		
 		const id = req.params.id;
 		const { remark, localQty } = req.body;
 		const repo = getRepository(ComponentPart);
+
+		await repo.findOneOrFail(id);
+
 		let componentPart = await repo.update(id, {
 			remark: remark,
 			localQty: localQty
 		});
-		res.send(true);
+		return res.send(true);
 	} catch(err) {
-		res.send(err.message);
+		next(err);
 	}
 }
 
-export async function getComponentPart(req, res) {
+export async function getComponentPart(req, res, next) {
 	try {
 		const id = req.params.id;
 		const repo = getRepository(ComponentPart);
 		let componentPart = await repo.findOneOrFail(id);
-		res.send(componentPart);
+		return res.send(componentPart);
 	} catch(err) {
-		res.send(err.message);
+		next(err);
 	}
 }
--- src/relationships/component-part/component-part.route.js
+++ src/relationships/component-part/component-part.route.js
@@ -2,12 +2,13 @@ import express from 'express';
 import perms from './component-part-permission';
 import { deleteComponentPart, editComponentPart, getComponentPart } from './component-part.controller';
 import { isAuthenticated } from '../../auth/auth';
+import validate from '../../validation/component-part.validation';
 
 const router = express.Router();
 
 // PREFIX: /component-parts
 router.delete('/:id', isAuthenticated(perms.delete), deleteComponentPart);
-router.put('/:id', isAuthenticated(perms.edit), editComponentPart);
+router.put('/:id', isAuthenticated(perms.edit), validate, editComponentPart);
 router.get('/:id', isAuthenticated(perms.get), getComponentPart);
 
 export default router;
